46d81351ce29ccb49b2d87707943e25c8313b418
yokai <driweman755@gmail.com>
2025-05-24 23:38:15 +0300

    get_date

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
 from masks import get_mask_card_number, get_mask_account
+from datetime import datetime
 
 def mask_account_card(data: str) -> str:
     """
@@ -22,3 +23,19 @@ def mask_account_card(data: str) -> str:
         raise ValueError("Номер должен содержать только цифры.")
 
     return f"{prefix} {masked}"
+
+
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразует строку даты из формата ISO 'ГГГГ-ММ-ДДT02:26:18.671407'
+    в формат 'ДД.ММ.ГГГГ'.
+
+    :param date_str: строка с датой в формате ISO
+    :return: строка с датой в формате 'ДД.ММ.ГГГГ'
+    """
+    try:
+        dt = datetime.fromisoformat(date_str)
+        return dt.strftime("%d.%m.%Y")
+    except ValueError:
+        raise ValueError("Некорректный формат даты. Ожидается 'YYYY-MM-DDTHH:MM:SS.ssssss'")
